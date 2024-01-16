5064f13389541b8b2af9957e1ba1286b9ac8a062
n.mishonin <n.mishonin.tonnus@visotsky.com>
2024-01-16 18:44:11 +0600

    init commit

---
+++ TimeDoctorAlert/ApiWrapper.cs
@@ -0,0 +1,162 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Serilog;
+
+namespace TimeDoctorAlert;
+
+public class ApiWrapper
+{
+    public class WindowInfo
+    {
+        public IntPtr Handle { get; set; }
+        public string Title { get; set; }
+        public string ProcessName { get; set; }
+        public RECT Rect { get; set; }
+        public string ClassName { get; set; }
+        public bool IsForeground { get; set; }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct LASTINPUTINFO
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [DllImport("user32.dll")]
+    static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool EnumWindows(EnumWindowsProc enumFunc, IntPtr lParam);
+
+    [DllImport("user32.dll")]
+    static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+    [DllImport("user32.dll")]
+    static extern int GetWindowTextLength(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+    public static List<WindowInfo> GetAllWindows()
+    {
+        var windows = new List<WindowInfo>();
+        EnumWindows(delegate (IntPtr hWnd, IntPtr lParam)
+        {
+            if (IsWindowVisible(hWnd))
+            {
+                var windowInfo = new WindowInfo();
+                windowInfo.Handle = hWnd;
+
+                var sb = new StringBuilder(GetWindowTextLength(hWnd) + 1);
+                GetWindowText(hWnd, sb, sb.Capacity);
+                windowInfo.Title = sb.ToString();
+
+                GetWindowRect(hWnd, out RECT rect);
+                windowInfo.Rect = rect;
+
+                uint processId;
+                GetWindowThreadProcessId(hWnd, out processId);
+                var process = Process.GetProcessById((int)processId);
+                windowInfo.ProcessName = process.ProcessName;
+
+                StringBuilder className = new StringBuilder(256);
+                GetClassName(hWnd, className, className.Capacity);
+                windowInfo.ClassName = className.ToString();
+
+                windowInfo.IsForeground = (hWnd == GetForegroundWindow());
+
+                windows.Add(windowInfo);
+            }
+
+            return true;
+        }, IntPtr.Zero);
+
+        return windows;
+    }
+    public static void LogWindow(string action, WindowInfo window)
+    {
+        var size = $"{window.Rect.Right - window.Rect.Left}x{window.Rect.Bottom - window.Rect.Top}";
+        var position = $"{window.Rect.Left};{window.Rect.Top}";
+
+        Log.Information("[{Action}]  Handle: {Handle}, Title: {Title}, Process: {ProcessName}, Size: {Size}, Position: {Position}, Class: {ClassName}, IsForeground: {IsForeground}",
+            action, window.Handle, window.Title, window.ProcessName, size, position, window.ClassName, window.IsForeground);
+    }
+
+    private List<WindowInfo> _windows = new();
+    public int UpdateWindowList(Func<WindowInfo, bool> query)
+    {
+        var snapshot = GetAllWindows()
+            .Where(query)
+            .ToList();
+
+        var newWindows = snapshot
+            .Where(w => _windows.All(w2 => w2.Handle != w.Handle))
+            .ToList();
+
+        var closedWindows = _windows
+            .Where(w => snapshot.All(w2 => w2.Handle != w.Handle))
+            .ToList();
+
+        var changedWindows = snapshot
+            .Where(w => _windows.Any(w2 => w2.Handle == w.Handle && w2.IsForeground != w.IsForeground))
+            .ToList();
+
+        if (newWindows.Count != 0 || closedWindows.Count != 0 || changedWindows.Count != 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            foreach (var window in newWindows) 
+                LogWindow("OPEN",  window);
+
+            foreach (var window in closedWindows)
+                LogWindow("CLOSE", window);
+
+            foreach (var window in changedWindows)
+                LogWindow("CHANGE", window);
+
+            _windows = snapshot;
+
+            Log.Information("Windows count: {Count}", _windows.Count);
+        }
+
+        return _windows.Count;
+    }
+    public static TimeSpan GetIdleTime()
+    {
+        var lastInputInfo = new LASTINPUTINFO
+        {
+            cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO))
+        };
+
+        if (!GetLastInputInfo(ref lastInputInfo))
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+
+        return TimeSpan.FromMilliseconds(Environment.TickCount - lastInputInfo.dwTime);
+    }
+}
---
+++ TimeDoctorAlert/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Media;
+using System.Windows;
+using System.Windows.Forms;
+using Serilog;
+using static TimeDoctorAlert.ApiWrapper;
+using Window = System.Windows.Window;
+
+
+namespace TimeDoctorAlert
+{
+    public partial class MainWindow : Window
+    {
+        private readonly NotifyIcon _trayIcon;
+        private readonly ApiWrapper _wrapper = new();
+
+        private int _windowsCount;
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+        private readonly Func<WindowInfo, bool>  _filter = w => w.IsForeground && w.ProcessName == "Time Doctor";
+
+        public MainWindow()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            InitializeComponent();
+            Hide();
+
+            _trayIcon = new NotifyIcon
+            {
+                Icon = Properties.Resources.logo,
+                Visible = true
+            };
+
+            _trayIcon.DoubleClick += (sender, args) =>
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+
+            Closed += MainWindow_Closed;
+
+            Task.Run(() => MonitorWindow(_cancellationTokenSource.Token));
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e) => _cancellationTokenSource.Cancel();
+
+        private async Task MonitorWindow(CancellationToken cancellationToken)
+        {
+            while (cancellationToken.IsCancellationRequested == false)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var windowsCount = _wrapper.UpdateWindowList(_filter);
+
+                if (windowsCount > _windowsCount)
+                    await CheckActivityAndPlaySound(cancellationToken);
+
+                if (windowsCount != _windowsCount)
+                {
+                    Log.Information("Set windows count: {Count}", windowsCount);
+                    _windowsCount = windowsCount;
+                }
+
+                await Task.Delay(500, cancellationToken);
+            }
+        }
+        private async Task CheckActivityAndPlaySound(CancellationToken cancellationToken)
+        {
+            var idleThreshold = TimeSpan.FromSeconds(1);
+
+            var counter = 0;
+
+            const int beepDelay = 1000;
+            const int maxCounter = 90;
+
+            while (GetIdleTime() > idleThreshold)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                SystemSounds.Hand.Play();
+
+                var windowsCount = _wrapper.UpdateWindowList(_filter);
+                if (windowsCount <= _windowsCount)
+                {
+                    Log.Information("Time Doctor window closed, {Count}", windowsCount);
+                    break;
+                }
+
+                if (counter++ > maxCounter)
+                    break;
+
+                await Task.Delay(beepDelay, cancellationToken);
+            }
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized) Hide();
+            base.OnStateChanged(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _trayIcon.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
